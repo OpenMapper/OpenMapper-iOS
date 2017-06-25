69861a379d27a59895efded00a1d541f3083539c
Nicolas Degen <ndegen@me.com>
2017-06-25 12:31:06 +0200

    building and linking to OpenMapper library

---
+++ Resources/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  OpenMapper
+//
+//  Created by Nicolas Degen on 25.06.17.
+//  Copyright © 2017 OpenMapper. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
